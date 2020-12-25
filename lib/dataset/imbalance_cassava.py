dd585eff3dc51a85c79bf86b9cd06a9dc4aa36c3
Our_wlsg <zhucc97@gmail.com>
2020-12-25 10:31:06 +0800

    updata

--- lib/dataset/imbalance_cassava.py
+++ lib/dataset/imbalance_cassava.py
@@ -108,21 +108,21 @@ class IMBALANCECASSAVA(Dataset):
             self.class_dict = self._get_class_dict()
 
     # cls_num=5, imb_type='exp', imb_factor=0.02
-    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
-        img_max = len(self.data) / cls_num
-        img_num_per_cls = []
-        if imb_type == 'exp':
-            for cls_idx in range(cls_num):
-                num = img_max * (imb_factor ** (cls_idx / (cls_num - 1.0)))
-                img_num_per_cls.append(int(num))
-        elif imb_type == 'step':
-            for cls_idx in range(cls_num // 2):
-                img_num_per_cls.append(int(img_max))
-            for cls_idx in range(cls_num // 2):
-                img_num_per_cls.append(int(img_max * imb_factor))
-        else:
-            img_num_per_cls.extend([int(img_max)] * cls_num)
-        return img_num_per_cls
+    # def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+    #     img_max = len(self.data) / cls_num
+    #     img_num_per_cls = []
+    #     if imb_type == 'exp':
+    #         for cls_idx in range(cls_num):
+    #             num = img_max * (imb_factor ** (cls_idx / (cls_num - 1.0)))
+    #             img_num_per_cls.append(int(num))
+    #     elif imb_type == 'step':
+    #         for cls_idx in range(cls_num // 2):
+    #             img_num_per_cls.append(int(img_max))
+    #         for cls_idx in range(cls_num // 2):
+    #             img_num_per_cls.append(int(img_max * imb_factor))
+    #     else:
+    #         img_num_per_cls.extend([int(img_max)] * cls_num)
+    #     return img_num_per_cls
 
     def sample_class_index_by_weight(self):
         rand_number, now_sum = random.random() * self.sum_weight, 0
@@ -217,23 +217,23 @@ class IMBALANCECASSAVA(Dataset):
             annos.append({'category_id': int(target)})
         return annos
 
-    def gen_imbalanced_data(self, img_num_per_cls):
-        new_data = []
-        new_targets = []
-        targets_np = np.array(self.targets, dtype=np.int64)
-        classes = np.unique(targets_np)
-        # np.random.shuffle(classes)
-        self.num_per_cls_dict = dict()
-        for the_class, the_img_num in zip(classes, img_num_per_cls):
-            self.num_per_cls_dict[the_class] = the_img_num
-            idx = np.where(targets_np == the_class)[0]
-            np.random.shuffle(idx)
-            selec_idx = idx[:the_img_num]
-            new_data.append(self.data[selec_idx, ...])
-            new_targets.extend([the_class, ] * the_img_num)
-        new_data = np.vstack(new_data)
-        self.data = new_data
-        self.targets = new_targets
+    # def gen_imbalanced_data(self, img_num_per_cls):
+    #     new_data = []
+    #     new_targets = []
+    #     targets_np = np.array(self.targets, dtype=np.int64)
+    #     classes = np.unique(targets_np)
+    #     # np.random.shuffle(classes)
+    #     self.num_per_cls_dict = dict()
+    #     for the_class, the_img_num in zip(classes, img_num_per_cls):
+    #         self.num_per_cls_dict[the_class] = the_img_num
+    #         idx = np.where(targets_np == the_class)[0]
+    #         np.random.shuffle(idx)
+    #         selec_idx = idx[:the_img_num]
+    #         new_data.append(self.data[selec_idx, ...])
+    #         new_targets.extend([the_class, ] * the_img_num)
+    #     new_data = np.vstack(new_data)
+    #     self.data = new_data
+    #     self.targets = new_targets
 
     def get_cls_num_list(self):
         cls_num_list = []
