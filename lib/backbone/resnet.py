996e0add3507245fcec4c294b9b56a6ce3de2d9c
Our_wlsg <zhucc97@gmail.com>
2020-12-25 10:52:24 +0800

    updat resnet.py 293hang

--- lib/backbone/resnet.py
+++ lib/backbone/resnet.py
@@ -290,6 +290,7 @@ def bbn_res50(
         last_layer_stride=last_layer_stride,
     )
     if pretrain and pretrained_model != "":
+        resnet = torch.nn.DataParallel(resnet)
         resnet.load_model(pretrain=pretrained_model)
     else:
         print("Choose to train from scratch")
